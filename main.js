54dbbceb476fac8d290f64224b123047ae4a4352
MaoScut <849345372@qq.com>
2017-05-12 08:30:45 +0800

    完成功能，还需提升，这个只能看一次就退出了。

--- main.js
+++ main.js
@@ -1,31 +1,67 @@
-var fs=require('fs');
-fs.readdir(process.cwd(), function(err, files){
+var fs=require('fs'),
+	stdin=process.stdin,
+	stdout=process.stdout;
+var stats=[];
+var files;
+function file(i){
+	var fileName = files[i];
+
+	fs.stat(__dirname+'/'+fileName, function(err, stat){
+		stats[i]=stat;
+		if(stat.isDirectory()){
+			console.log('		'+i+'	\033[36m'+fileName+'/\033[39m');
+		}else{
+			console.log('		'+i+'	\033[90m'+fileName+'\033[39m');
+		}
+
+		if(++i==files.length){
+			// console.log(' ');
+			// process.stdout.write('	\033[33mEnter your choice: \033[39m');
+			// process.stdin.resume();
+			// process.stdin.setEncoding('utf8');
+			read();
+		}else{
+			file(i);
+		}
+	})
+}
+function read(){
+	console.log(' ');
+	stdout.write('	\033[33mEnter your choice: \033[39m');
+	stdin.resume();
+	stdin.setEncoding('utf8');
+	stdin.on('data', option);
+}
+function option(data){
+	if(!files[Number(data)]){
+		stdout.write('	\033[31mEnter your choice!: \033[39m');
+	}else {
+		stdin.pause();
+		if(stats[Number(data)].isDirectory()){
+			fs.readdir(__dirname+'/'+files[Number(data)], function(err, files){
+				console.log(' ');
+				console.log('		('+files.length+' files)');
+				files.forEach(function(file){
+					console.log('		-    '+file);
+					console.log(' ');
+				})
+			})
+		}else {
+			fs.readFile(__dirname+'/'+files[Number(data)],'utf8', function(err,data){
+				console.log(' ');
+				console.log('\033[90m'+data+'\033[39m');
+			})
+		}
+	}
+}
+fs.readdir(process.cwd(), function(err, _files){
 	console.log(' ');
 	if(!file.length){
 		return console.log('	\033[31m No files to show!\033[39m\n')
 	}
-
+	files=_files;
 	console.log('		Select which file or directory you want to see\n');
-	function file(i){
-		var fileName = files[i];
 
-		fs.stat(__dirname+'/'+fileName, function(err, stat){
-			if(stat.isDirectory()){
-				console.log('		'+i+'	\033[36m'+fileName+'	/\033[39m');
-			}else{
-				console.log('		'+i+'	\033[90m'+fileName+'	\033[39m');
-			}
+	file(0,files);
+});
 
-			i++;
-			if(i==files.length){
-				console.log(' ');
-				process.stdout.write('	\033[33mEnter your choice: \033[39m');
-				process.stdin.resume();
-				process.stdin.setEncoding('utf8');
-			}else{
-				file(i);
-			}
-		})
-	}
-	file(0)
-})
